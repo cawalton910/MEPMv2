41e7e11c32d25c1bdb11f2e064867ce7a7df751e
BuildTools <unconfigured@null.spigotmc.org>
2022-08-11 19:15:39 -0400

    2

--- wise/server.js
+++ wise/server.js
@@ -1,8 +1,11 @@
-var http = require('http');
-var fs = require('fs');
-var index = fs.readFileSync('index.html');
+var http = require('http'); // 1 - Import Node.js core module
 
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end(index);
-}).listen(8080);
+var server = http.createServer(function (req, res) {   // 2 - creating server
+
+    //handle incomming requests here..
+
+});
+
+server.listen(5000); //3 - listen for any incoming requests
+
+console.log('Node.js web server at port 5000 is running..')
